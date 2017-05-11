81c8256f46831b49ccb874ca5509f7de29ea01ab
JulioBarahona <julio.fbm96@gmail.com>
2017-05-10 21:19:57 -0600

    added git ignore
    .class and .log files included

--- src/Persona.java
+++ src/Persona.java
@@ -15,14 +15,8 @@ public class Persona {
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
-	
-	
-	public Persona(String nombre, String apellido) {
-		super();
-		this.nombre = nombre;
-		this.apellido = apellido;
-	}
-	
+
+	public 
 	
 
 }
