56c45911aebf54e67f312c8fb9bade848a4654a2
joiceann <mirandandrea19@gmail.com>
2017-05-24 19:20:31 -0600

    Add files via upload

--- Sprint 2/Android Studio/PHP Login-Register AS/Test Encrypted DB/Register.php
+++ Sprint 2/Android Studio/PHP Login-Register AS/Test Encrypted DB/Register.php
@@ -1,44 +1,17 @@
 <?php
-    require("password.php");
-
-    $connect = mysqli_connect("localhost", "id1757928_joseantogt", "jas3ikrr", "id1757928_horasbecaextension");
+    $con = mysqli_connect("localhost", "id1757928_joseantogt", "jas3ikrr", "id1757928_horasbecaextension");
     
     $name = $_POST["name"];
     $age = $_POST["age"];
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-     function registerUser() {
-        global $connect, $name, $age, $username, $password;
-        $passwordHash = password_hash($password, PASSWORD_DEFAULT);
-        $statement = mysqli_prepare($connect, "INSERT INTO user (name, age, username, password) VALUES (?, ?, ?, ?)");
-        mysqli_stmt_bind_param($statement, "siss", $name, $age, $username, $passwordHash);
-        mysqli_stmt_execute($statement);
-        mysqli_stmt_close($statement);     
-    }
-
-    function usernameAvailable() {
-        global $connect, $username;
-        $statement = mysqli_prepare($connect, "SELECT * FROM user WHERE username = ?"); 
-        mysqli_stmt_bind_param($statement, "s", $username);
-        mysqli_stmt_execute($statement);
-        mysqli_stmt_store_result($statement);
-        $count = mysqli_stmt_num_rows($statement);
-        mysqli_stmt_close($statement); 
-        if ($count < 1){
-            return true; 
-        }else {
-            return false; 
-        }
-    }
-
+    $statement = mysqli_prepare($con, "INSERT INTO user (name, username, age, password) VALUES (?, ?, ?, ?)");
+    mysqli_stmt_bind_param($statement, "siss", $name, $username, $age, $password);
+    mysqli_stmt_execute($statement);
+    
     $response = array();
-    $response["success"] = false;  
-
-    if (usernameAvailable()){
-        registerUser();
-        $response["success"] = true;  
-    }
+    $response["success"] = true;  
     
     echo json_encode($response);
 ?>
